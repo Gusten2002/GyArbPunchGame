91be5a97c398b017b765ffa60b1387c3122a9a97
Pontus <02pontuskarlsson@gmail.com>
2020-09-25 13:29:36 +0200

    MediumButton tillagd

---
+++ Assets/MediumButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediumButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random generator = new Random();
+        int attack = 6;
+        int attackChance = generator.Next(10);
+
+        for (int i = 0; i < 10; i + 2)
+        {
+            if (attackChance == i)
+            {
+                
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
