a64289a11d28515fdf4f5bc02a3fe0bd456fdcf6
August <august.agren@elev.ga.ntig.se>
2020-09-25 13:07:40 +0200

    SmallDude har hp nu

--- Assets/SmallEnemieScript.cs
+++ Assets/SmallEnemieScript.cs
@@ -7,12 +7,16 @@ public class SmallEnemieScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int smallDudeHp = 20;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (smallDudeHp <= 0)
+        {
+            Destroy(This.GameObject);
+        }
     }
 }
