087bd9ad6f89d5500f76408aa798cb46652aec74
Pontus <02pontuskarlsson@gmail.com>
2020-10-09 13:27:33 +0200

    Hearts added

---
+++ Assets/HPHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpHandler = GameObject.Find("Heart (0)").GetComponent<HPhandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void HPCheck()
+    {
+
+    }
+}
